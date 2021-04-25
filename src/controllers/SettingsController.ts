4b4aea4b8c4f078c2be9834d10b49536cdebcf0d
Vitor Saraiva <55057661+SaraivaVitor@users.noreply.github.com>
2021-04-25 19:52:41 -0300

    Finalização do projeto

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,20 +1,43 @@
 import { Request, Response } from "express"
-import { getCustomRepository } from "typeorm"
-import { SettingsRepository } from "../repositories/SettingsRepository"
+import { SettingsService } from "../services/SettingsService"
 
 class SettingsController{
     async create(request:Request, response:Response){
-        const settingsRepository = getCustomRepository(SettingsRepository)
         const { chat, username } = request.body
+
+        const settingsService =  new SettingsService()
+
+        try{
+
+            const settings = await settingsService.create({ chat, username })
+        
+            return response.json(settings)
+
+        }catch(err){
+            return response.status(400).json({
+                message: err.message
+            })
+        }
+    }
+    async findByUsername(req: Request, res: Response) {
+        const { username } = req.params
     
-        const settings = settingsRepository.create({
-            chat,
-            username
-        })
+        const settingsService = new SettingsService()
     
-        await settingsRepository.save(settings)
+        const setting = await settingsService.findByUsername(username)
     
-        return response.json(settings)
+        return res.json(setting)
+    }
+
+    async update(req: Request, res: Response) {
+        const { username } = req.params
+        const { chat } = req.body
+    
+        const settingsService = new SettingsService()
+    
+        const setting = await settingsService.update(username, chat)
+    
+        return res.json(setting)
     }
 }
 
